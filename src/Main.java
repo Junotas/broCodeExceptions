80cff5747cb3075de8c96b58b5716d701257a865
Ludvig <ludvig.fendert@appliedtechnology.se>
2024-05-05 13:55:03 +0200

    scanner for two numbers to be divided and a try and catch for if the user enters a zero

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
 //exception = an event that disrupts the programs flow of instructions
+try {
+    Scanner scanner = new Scanner(System.in);
 
+    System.out.println("Enter whole number to divide: ");
+    int x = scanner.nextInt();
 
+    System.out.println("Enter whole number to divide by: ");
+    int y = scanner.nextInt();
 
+    int z = x / y;
 
+    System.out.println("result" + z);
+}
+    catch (ArithmeticException e) {
+    System.out.println("You can't divide by zero idiot");
 
+}
     }
 }
